8bab7aba5d288f0eaa5f8d7bbfa037ffbccf5dbe
piyushgiitian <piyushgautamtg@gmail.com>
2022-04-03 16:12:42 +0530

    initial deploy

---
+++ commands.py
@@ -0,0 +1,11 @@
+from flask.cli import with_appcontext
+import click
+
+from posts.controller import db
+
+
+@click.command(name="create_tables")
+
+@with_appcontext
+def create_tables():
+    db.create_all()
